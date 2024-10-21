31aa06c18f01aae3eb2e669624623cf24f3b278a
Abdul Moazzim <abdulmoazzam0815@gmail.com>
2024-10-21 22:26:11 +0500

    Half remaining

--- multi-step-form-main/script.js
+++ multi-step-form-main/script.js
@@ -1,136 +1,200 @@
+"use strict";
+let count = 0;
 let html1 = [
-  `<div id="content1">
-      <div id="step1Info">
-        <h1>Personal Info</h1>
-        <p>Please provide your name, email address, and phone number.</p>
-        <label for="name">Name</label>
-        <input type="text" id="name" class="info">
-        <label for="email">Email Address</label>
-        <input type="email" id="email" class="info">
-        <label for="number">Phone Number</label>
-        <input type="number" id="number" class="info">
-      </div>
-      <div id="next1a">
-        <button id="btn1">Next Step</button>
-      </div>
-    </div>`,
-
-  `<div id="content2">
-      <div id="step2Info">
-        <h1>Select your plan</h1>
-        <p id="option">You have the option of monthly or yearly billing.</p>
-        <div class="MyplansA">
-          <div class="plansA">
-            <img src="assets/images/icon-arcade.svg" alt="arcade">
-            <div>
-              <p class="heading">Arcade</p>
-              <p class="txt">$9/mo</p>
-            </div>
-          </div>
-          <div class="plansA">
-            <img src="assets/images/icon-advanced.svg" alt="advanced">
-            <div>
-              <p class="heading">Advaqnced</p>
-              <p class="txt">$12/mo</p>
-            </div>
-          </div>
-          <div class="plansA">
-            <img src="assets/images/icon-pro.svg" alt="pro">
-            <div>
-              <p class="heading">Pro</p>
-              <p class="txt">$15/mo</p>
-            </div>
-          </div>
-        </div>
-        <div id="ways">
-          <p class="way">Monthly</p>
-          <div>
-            <div id="planBtn">
-              <div></div>
-            </div>
-          </div>
-          <p class="way">Yearly</p>
-        </div>
-        
-      </div>
-      <div id="next1b">
-        <button id="goBack">Go Back</button>
-        <button id="btn1">Next Step</button>
-      </div>
-    </div>`,
-
-  `<div id="content3">
-      <div id="step3Info">
-        <h1>Pick add-ons</h1>
-        <p id="option">Add-ons help enhance your gaming experience.</p>
-        <div class="MyplansB">
-          <div class="plansB">
-            <div class="heads">
-              <div class="tickBox">
-                <input type="checkbox" id="">
-              </div>
-              <div>
-                <p class="heading">Online service</p>
-                <p class="txt">Access to multiplayer games</p>
-              </div>
-            </div>
-            <div>+1$/mo</div>
-          </div>
-          <div class="plansB">
-            <div class="heads">
-              <div class="tickBox">
-                <input type="checkbox" id="">
-              </div>
-              <div>
-                <p class="heading">Larger storage</p>
-                <p class="txt">Extra 1TB of cloud save</p>
-              </div>
-            </div>
-            <div>+2$/mo</div>
-          </div>
-          <div class="plansB">
-            <div class="heads">
-              <div class="tickBox">
-                <input type="checkbox" id="">
-              </div>
-              <div>
-                <p class="heading">Customizable profile</p>
-                <p class="txt">Customize theme on your profile</p>
-              </div>
-            </div>
-            <div>+2$/mo</div>
-          </div>
-        </div>
-        <div id="ways">
-          <p class="way">Monthly</p>
-          <div>
-            <div id="planBtn">
-              <div></div>
-            </div>
-          </div>
-          <p class="way">Yearly</p>
-        </div>
-        
-      </div>
-      <div id="next1b">
-        <button id="goBack">Go Back</button>
-        <button id="btn1">Next Step</button>
-      </div>
-    </div>`,
+    `<h1>Personal Info</h1>
+  <p>Please provide your name, email address, and phone number.</p>
+  <label for="name">Name</label>
+  <input type="text" id="name" class="info">
+  <label for="email">Email Address</label>
+  <input type="email" id="email" class="info">
+  <label for="number">Phone Number</label>
+  <input type="number" id="number" class="info">`,
+    `<h1>Select your plan</h1>
+  <p id="option">You have the option of monthly or yearly billing.</p>`,
+    `<h1>Pick add-ons</h1>
+  <p id="option">Add-ons help enhance your gaming experience.</p>`,
 ];
-
-let html2 = [
-  ` <div id="next2a">
-    <button id="btn2">Next Step</button>
-  </div>`,
-
-  `<div id="next2b">
-  <button id="goBack">Go Back</button>
-  <button id="btn2">Next Step</button>
-</div>`,
-
-  `<div id="next2b">
-  <button id="goBack">Go Back</button>
-  <button id="btn2">Next Step</button>
-</div>`,
-];
+function func() {
+    if (circles) {
+        circles.forEach((val, index) => {
+            const element = val;
+            if (count == index) {
+                element.style.backgroundColor = "hsl(206, 94%, 87%)";
+                element.style.color = "hsl(213, 96%, 18%)";
+            }
+            else {
+                element.style.backgroundColor = "transparent";
+                element.style.color = "white";
+            }
+        });
+    }
+    if (myDiv) {
+        myDiv.innerHTML = html1[count];
+    }
+    if (count > 0 && count < 3) {
+        goBack1.style.display = "block";
+        goBack2.style.display = "block";
+        Confirm.style.display = "none";
+        next1.style.display = "block";
+        next2.style.display = "block";
+    }
+    else if (count > 2) {
+        Confirm.style.display = "block";
+        next1.style.display = "none";
+        next2.style.display = "none";
+    }
+    else {
+        goBack1.style.display = "none";
+        goBack2.style.display = "none";
+        next1.style.display = "block";
+        next2.style.display = "block";
+    }
+}
+let goBack1 = document.querySelector("#goBack1");
+let goBack2 = document.querySelector("#goBack2");
+let next1 = document.querySelector("#btn1");
+let next2 = document.querySelector("#btn2");
+let circles = document.querySelectorAll(".circles");
+let myDiv = document.querySelector("#stepInfo");
+let Confirm = document.querySelector("#confirm");
+let planA = document.querySelector("#MyplansA");
+let planB = document.querySelector("#MyplansB");
+let plansA = document.querySelectorAll(".plansA");
+let plansB = document.querySelectorAll(".plansB");
+let headingA = document.querySelectorAll(".headingA");
+let headingB = document.querySelectorAll(".headingB");
+let tick = document.querySelectorAll(".tick");
+let txt1 = document.querySelectorAll(".txt1");
+let txt2 = document.querySelectorAll(".txt2");
+let free = document.querySelectorAll(".free");
+let planBtn = document.querySelector("#planBtn");
+let ways = document.querySelector("#ways");
+let cir = document.querySelector("#cir");
+function next() {
+    if (count < 4) {
+        count++;
+        func();
+        if (count == 1) {
+            ways.style.display = "flex";
+            planA.style.display = "flex";
+            planB.style.display = "none";
+        }
+        else if (count == 2) {
+            ways.style.display = "none";
+            planB.style.display = "block";
+            planA.style.display = "none";
+        }
+        else {
+            ways.style.display = "none";
+            planB.style.display = "none";
+            planA.style.display = "none";
+        }
+    }
+}
+function previous() {
+    if (count > -1) {
+        count--;
+        func();
+        if (count == 1) {
+            ways.style.display = "flex";
+            planA.style.display = "flex";
+            planB.style.display = "none";
+        }
+        else if (count == 2) {
+            ways.style.display = "none";
+            planB.style.display = "block";
+            planA.style.display = "none";
+        }
+        else {
+            ways.style.display = "none";
+            planA.style.display = "none";
+            planB.style.display = "none";
+        }
+    }
+}
+if (next1) {
+    next1.addEventListener("click", next);
+}
+if (next2) {
+    next2.addEventListener("click", next);
+}
+if (goBack1) {
+    goBack1.addEventListener("click", previous);
+}
+if (goBack2) {
+    goBack2.addEventListener("click", previous);
+}
+let paid1 = [[9, 12, 15], [90, 120, 150]];
+let paid2 = [[1, 2, 2], [10, 20, 20]];
+let counting = 0;
+if (planBtn) {
+    planBtn.addEventListener('click', () => {
+        if (counting == 0) {
+            cir.style.left = "45%";
+            txt1.forEach((val, index) => {
+                const element = val;
+                element.innerHTML = `$${paid1[1][index]}/yr`;
+            });
+            txt2.forEach((val, index) => {
+                const element = val;
+                element.innerHTML = `+$${paid2[0][index]}/yr`;
+            });
+            free.forEach((val) => {
+                const element = val;
+                element.style.display = "block";
+            });
+            counting = 1;
+        }
+        else {
+            cir.style.left = "0%";
+            txt1.forEach((val, index) => {
+                const element = val;
+                element.innerHTML = `$${paid1[0][index]}/mo`;
+            });
+            txt2.forEach((val, index) => {
+                const element = val;
+                element.innerHTML = `+$${paid2[1][index]}/mo`;
+            });
+            free.forEach((val) => {
+                const element = val;
+                element.style.display = "none";
+            });
+            counting = 0;
+        }
+    });
+}
+let obj1;
+let obj2;
+plansA.forEach((val, index) => {
+    val.addEventListener('click', () => {
+        plansA.forEach((val1) => {
+            val1.classList.remove("clickOnA");
+            val1.classList.add("clickOffA");
+        });
+        if (val.classList.contains("clickOnA")) {
+            val.classList.remove("clickOnA");
+            val.classList.add("clickOffA");
+        }
+        else {
+            val.classList.remove("clickOffA");
+            val.classList.add("clickOnA");
+        }
+        obj1 = [{ package: headingA[index].innerHTML, price: txt1[index].innerHTML }];
+    });
+});
+tick.forEach((val, index) => {
+    val.addEventListener('click', () => {
+        if (plansB[index].classList.contains("clickOnB") && txt2[index].classList.contains("txtpurple")) {
+            plansB[index].classList.remove("clickOnB");
+            plansB[index].classList.add("clickOffB");
+            txt2[index].classList.remove("txtpurple");
+            txt2[index].classList.add("txtblack");
+        }
+        else {
+            plansB[index].classList.remove("clickOffB");
+            plansB[index].classList.add("clickOnB");
+            txt2[index].classList.remove("txtblack");
+            txt2[index].classList.add("txtpurple");
+        }
+    });
+});
---
+++ multi-step-form-main/script.ts
@@ -0,0 +1,222 @@
+let count: number = 0;
+
+let html1 = [
+  `<h1>Personal Info</h1>
+  <p>Please provide your name, email address, and phone number.</p>
+  <label for="name">Name</label>
+  <input type="text" id="name" class="info">
+  <label for="email">Email Address</label>
+  <input type="email" id="email" class="info">
+  <label for="number">Phone Number</label>
+  <input type="number" id="number" class="info">`,
+
+  `<h1>Select your plan</h1>
+  <p id="option">You have the option of monthly or yearly billing.</p>`,
+
+  `<h1>Pick add-ons</h1>
+  <p id="option">Add-ons help enhance your gaming experience.</p>`,
+];
+
+
+function func():void {
+  if (circles) {
+    circles.forEach((val, index) => {
+      const element = val as HTMLElement;
+      if (count == index) {
+        element.style.backgroundColor = "hsl(206, 94%, 87%)";
+        element.style.color = "hsl(213, 96%, 18%)";
+      } else {
+        element.style.backgroundColor = "transparent";
+        element.style.color = "white";
+      }
+    });
+  }
+  if (myDiv) {
+    myDiv.innerHTML = html1[count];
+  }
+  if (count > 0 && count < 3) {
+    goBack1.style.display = "block";
+    goBack2.style.display = "block";
+    Confirm.style.display = "none";
+    next1.style.display = "block";
+    next2.style.display = "block";
+  } else if (count > 2) {
+    Confirm.style.display = "block";
+    next1.style.display = "none";
+    next2.style.display = "none";
+  }
+  else {
+    goBack1.style.display = "none";
+    goBack2.style.display = "none";
+    next1.style.display = "block";
+    next2.style.display = "block";
+  }
+}
+ 
+
+
+let goBack1 = document.querySelector("#goBack1") as HTMLElement;
+let goBack2 = document.querySelector("#goBack2") as HTMLElement;
+let next1 = document.querySelector("#btn1") as HTMLElement;
+let next2 = document.querySelector("#btn2") as HTMLElement;
+let circles = document.querySelectorAll(".circles");
+let myDiv = document.querySelector("#stepInfo");
+let Confirm = document.querySelector("#confirm") as HTMLElement;
+let planA = document.querySelector("#MyplansA") as HTMLElement;
+let planB = document.querySelector("#MyplansB") as HTMLElement;
+let plansA = document.querySelectorAll(".plansA");
+let plansB = document.querySelectorAll(".plansB");
+let headingA = document.querySelectorAll(".headingA");
+let headingB = document.querySelectorAll(".headingB");
+let tick = document.querySelectorAll(".tick");
+let txt1 = document.querySelectorAll(".txt1");
+let txt2 = document.querySelectorAll(".txt2");
+let free = document.querySelectorAll(".free");
+let planBtn = document.querySelector("#planBtn") as HTMLElement;
+let ways = document.querySelector("#ways") as HTMLElement;
+let cir = document.querySelector("#cir") as HTMLElement;
+
+function next(): void {
+  if (count < 4) {
+    count++;
+    func();
+    if (count == 1) {
+      ways.style.display = "flex";
+      planA.style.display = "flex"
+      planB.style.display = "none"
+    }
+    else if (count == 2){
+      ways.style.display = "none";
+      planB.style.display = "block"
+      planA.style.display = "none"
+    }
+    else {
+      ways.style.display = "none";
+      planB.style.display = "none"
+      planA.style.display = "none"
+    }
+  }
+}
+
+function previous(): void {
+  if (count > -1) {
+    count--;
+    func();
+    if (count == 1) {
+      ways.style.display = "flex";
+      planA.style.display = "flex"
+      planB.style.display = "none"
+    }
+    else if (count == 2){
+      ways.style.display = "none";
+      planB.style.display = "block"
+      planA.style.display = "none"
+    }
+    else {
+      ways.style.display = "none";
+      planA.style.display = "none"
+      planB.style.display = "none"
+    }
+  }
+}
+
+
+if (next1) {
+  next1.addEventListener("click", next);
+}
+if (next2) {
+  next2.addEventListener("click", next);
+}
+if (goBack1) {
+  goBack1.addEventListener("click", previous);
+}
+if (goBack2) {
+  goBack2.addEventListener("click", previous);
+}
+
+let paid1: Number[][] = [[9,12,15],[90,120,150]];
+let paid2: Number[][]= [[1,2,2],[10,20,20]];
+
+let counting: Number = 0;
+if (planBtn) {
+  planBtn.addEventListener('click',()=>{
+    if (counting == 0) {
+      cir.style.left = "45%";
+      txt1.forEach((val,index)=>{
+        const element = val as HTMLElement;
+        element.innerHTML = `$${paid1[1][index]}/yr`;
+      })
+      txt2.forEach((val,index)=>{
+        const element = val as HTMLElement;
+        element.innerHTML = `+$${paid2[0][index]}/yr`
+      })
+      free.forEach((val)=>{
+        const element = val as HTMLElement;
+        element.style.display = "block";
+      })
+      counting = 1;
+    }
+    else {
+      cir.style.left = "0%";
+      txt1.forEach((val,index)=>{
+        const element = val as HTMLElement;
+        element.innerHTML = `$${paid1[0][index]}/mo`
+      })
+      txt2.forEach((val,index)=>{
+        const element = val as HTMLElement;
+        element.innerHTML = `+$${paid2[1][index]}/mo`
+      })
+      free.forEach((val)=>{
+        const element = val as HTMLElement;
+        element.style.display = "none";
+      })
+      counting = 0;
+    }
+    })
+}
+
+interface data1 {
+  package: string,
+  price: string
+}
+interface data2 {
+  package: string,
+  price: string
+}
+
+let obj1: data1[];
+let obj2: data2[];
+
+plansA.forEach((val,index)=>{
+  val.addEventListener('click',()=>{
+    plansA.forEach((val1)=>{
+      val1.classList.remove("clickOnA");
+      val1.classList.add("clickOffA");
+    })
+    if (val.classList.contains("clickOnA")) {
+      val.classList.remove("clickOnA");
+      val.classList.add("clickOffA");
+    }
+    else {
+      val.classList.remove("clickOffA");
+      val.classList.add("clickOnA");
+    }
+    obj1 = [{package: headingA[index].innerHTML, price: txt1[index].innerHTML}];
+  })
+})
+tick.forEach((val,index)=>{
+  val.addEventListener('click',()=>{
+    if (plansB[index].classList.contains("clickOnB") && txt2[index].classList.contains("txtpurple")) {
+      plansB[index].classList.remove("clickOnB");
+      plansB[index].classList.add("clickOffB");
+      txt2[index].classList.remove("txtpurple");
+      txt2[index].classList.add("txtblack");
+    }
+    else {
+      plansB[index].classList.remove("clickOffB");
+      plansB[index].classList.add("clickOnB");
+      txt2[index].classList.remove("txtblack");
+      txt2[index].classList.add("txtpurple");
+    }
+  })
+})
